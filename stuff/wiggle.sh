e47033653e1d431ea6946df41d6cf681d09d761f
Eric Xue <widefin@gmail.com>
2022-01-01 00:30:17 -0500

    Create wiggle.sh

---
+++ stuff/wiggle.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+while true; do
+	echo "wiggle"
+	sleep 0.1
+	echo " wiggle"
+        sleep 0.1
+	echo "  wiggle"
+        sleep 0.1
+	echo "    wiggle"
+        sleep 0.1
+	echo "        wiggle"
+        sleep 0.1
+	echo "            wiggle"
+        sleep 0.1
+	echo "              wiggle"
+        sleep 0.1
+	echo "               wiggle"
+        sleep 0.1
+	echo "                wiggle"
+        sleep 0.1
+	echo "               wiggle"
+        sleep 0.1
+	echo "              wiggle"
+        sleep 0.1
+	echo "            wiggle"
+        sleep 0.1
+	echo "        wiggle"
+        sleep 0.1
+	echo "    wiggle"
+	sleep 0.1
+	echo "  wiggle"
+        sleep 0.1
+	echo " wiggle"
+        sleep 0.1
+	done
+done
+
