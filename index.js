70e3bcbe0b180e920b5741cf352a6d1b74d5e800
Eric Xue <widefin@gmail.com>
2022-06-04 14:20:05 -0400

    good

---
+++ index.js
@@ -0,0 +1,19 @@
+function span_each(id, span_class){
+    var target = document.getElementById(id);
+    var len = target.innerHTML.length;
+    var new_str = "";
+
+    for (var i=0; i<len; ++i){
+        if (i < len-5 && target.innerHTML.substring(i, i+5)=="<span"){
+            while (i>6 && target.innerHTML.substring(i-7, i)!="</span>"){
+                new_str += target.innerHTML.charAt(i);
+                i++;
+            }
+        }
+        new_str += "<span class='" + span_class + "'>" + target.innerHTML.charAt(i) + "</span>";
+    }
+
+    target.innerHTML = new_str;
+}
+
+span_each("main-content", "term-char");
