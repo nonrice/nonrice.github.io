493417a132f220a080f6068ff8326d1d88624296
Eric Xue <widefin@gmail.com>
2022-06-04 14:49:15 -0400

    idk

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ function span_each(id, span_class){
     var new_str = "";
 
     for (var i=0; i<len; ++i){
+        // Retain original span tags
         if (i < len-5 && target.innerHTML.substring(i, i+5)=="<span"){
             while (i>6 && target.innerHTML.substring(i-7, i)!="</span>"){
                 new_str += target.innerHTML.charAt(i);
                 i++;
             }
         }
+        
         new_str += "<span class='" + span_class + "'>" + target.innerHTML.charAt(i) + "</span>";
     }
 
